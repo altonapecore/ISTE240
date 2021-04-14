bbe04af5ef715005884a1857df5438b8d7c9e9df
koobiie <kj8044@rit.edu>
2021-04-13 22:27:37 -0400

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,8 @@
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Wtf.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Wtf. I'm gonna assume
+        that the Discord bot is supposed to catch this?? Intersting? 
     </p>
 </div>
 
