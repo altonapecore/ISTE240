bd916109947b33842ac2fed49edf2c9bce8271db
Alex Pecore <33386022+altonapecore@users.noreply.github.com>
2021-05-06 21:39:38 -0400

    Microscopic small changes
    Why is there a violin here?

--- assets/php/header.php
+++ assets/php/header.php
@@ -3,18 +3,16 @@
 <head>
 	<title><?php echo $page ?></title>
     <link rel="stylesheet" href="<?php echo $path ?>assets/css/mainStyles.css">
-    <script src="<?php echo $path ?>assets/script.js"></script>
+    <script src="<?php echo $path ?>assets/js/script.js"></script>
 </head>
 <body onload = "changeLink()">
     <header>
         <a href="http://solace.ist.rit.edu/~iste240t44/"><img src="<?php echo $path ?>assets/img/logo.png"></img></a>
     <div id="navBar">
         <div class="flexContainer">
-            <!--Honestly not sure if home logo is 100% necessary-->
             <div class=flexNav><a href="http://solace.ist.rit.edu/~iste240t44/" id="active">Home</a></div>
             <div class=flexNav><a href="http://solace.ist.rit.edu/~iste240t44/assets/lessons/INTROkernel.php">Learn</a></div>
             <div class=flexNav><a href="http://solace.ist.rit.edu/~iste240t44/referenceAlphabet.php">Reference</a><div>
         </div>
-        <!--Dan suggested moving Our Team to footer so we can do that-->
     </div>
     </header>
